459f5f16c8b8f425a6a0890e290538103340fc5f
pallabofficial <officialpallabmondal000@gmail.com>
2024-02-08 13:04:55 +0530

    added animations to the circle on button  click

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-console.log("check");
 
-gsap.to("#circle", {
-    rotate:"30deg",
-    ease:"linear",
-    duration:2,
-    // repeat:1,
-    stagger:0,
+
+let buttons = document.querySelectorAll(".buttons");
+buttons.forEach(function(val,index){
+    val.addEventListener("click", function(){
+        gsap.to("#circle", {
+            rotate: (index-3+1)* (-30),
+            ease:"linear",
+            duration:2,
+        })
+    })
 })
